a6bb001cc2697a9e10ffcd215d72da3bc8f7a7c0
cg <cg>
2001-03-27 18:04:04 +0000

    Added macros and incline function to handle long long.

--- src/client_endian.h
+++ src/client_endian.h
@@ -35,15 +35,40 @@
 #define HW_TO_CW(hw) (hw)
 #define CL_TO_HL(cl) (cl)
 #define HL_TO_CL(hl) (hl)
+#define CLL_TO_HLL(cll) (cll)
+#define HLL_TO_CLL(hll) (hll)
 
 #else
 
 #include <netinet/in.h>
 
+static
+inline
+long long
+swap_long_long(long long ll)
+{
+  long long retval;
+
+  retval =
+    ((ll >> 56) & 0x00000000000000ff) |
+    ((ll >> 40) & 0x000000000000ff00) |
+    ((ll >> 24) & 0x0000000000ff0000) |
+    ((ll >>  8) & 0x00000000ff000000) |
+    ((ll <<  8) & 0x000000ff00000000) |
+    ((ll << 24) & 0x0000ff0000000000) |
+    ((ll << 40) & 0x00ff000000000000) |
+    ((ll << 56) & 0xff00000000000000);
+
+  return retval;
+}
+
+
 #define CW_TO_HW(cw) ntohs(cw)
 #define HW_TO_CW(hw) htons(hw)
 #define CL_TO_HL(cl) ntohl(cl)
 #define HL_TO_CL(hl) htonl(hl)
+#define CLL_TO_HLL(cll) swap_long_long(cll)
+#define HLL_TO_CLL(hll) swap_long_long(hll)
 
 #endif /* WORDS_BIGENDIAN */
 
